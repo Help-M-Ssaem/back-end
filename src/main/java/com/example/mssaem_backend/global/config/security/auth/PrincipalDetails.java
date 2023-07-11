c20851bfd3ca58f377e8d0735d62fc8d1424694c
유지수 <71329329+YooJisu826@users.noreply.github.com>
2023-07-11 20:05:25 +0900

    Spring Security 설정 (#35)

---
+++ src/main/java/com/example/mssaem_backend/global/config/security/auth/PrincipalDetails.java
@@ -0,0 +1,64 @@
+package com.example.mssaem_backend.global.config.security.auth;
+
+import com.example.mssaem_backend.domain.member.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+public class PrincipalDetails implements UserDetails {
+    private Member member;
+
+    public PrincipalDetails(Member member) {
+        this.member = member;
+    }
+
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public Member getMember() {
+        return this.member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(member.getRole().toString()));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return encodePwd().encode("this is password");
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return !member.isStatus();
+    }
+}
---
+++ src/main/java/com/example/mssaem_backend/global/config/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,36 @@
+package com.example.mssaem_backend.global.config.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends GenericFilterBean {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        // refresh Token이 있다면 refresh Token 사용
+        String refreshToken = jwtTokenProvider.resolveRefreshToken((HttpServletRequest) request);
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+        if (refreshToken != null && ((HttpServletRequest) request).getRequestURI()
+                .equals("/common/refresh") && jwtTokenProvider.validateRefreshToken(refreshToken)) {
+            Authentication authentication = jwtTokenProvider.getRefreshAuthentication(refreshToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } else if (token != null && jwtTokenProvider.validateToken(token)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        chain.doFilter(request, response);
+    }
+}
