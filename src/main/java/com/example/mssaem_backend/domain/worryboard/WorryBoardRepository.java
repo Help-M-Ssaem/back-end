551491f8a83862fd1e750dc06110f7560f49ac06
yulee <dbfl0461@gmail.com>
2023-07-19 16:05:04 +0900

    Feat : 홈 화면 조회 - M쌤 매칭을 기다리는 고민 #67

--- src/main/java/com/example/mssaem_backend/domain/worryboard/WorryBoardRepository.java
+++ src/main/java/com/example/mssaem_backend/domain/worryboard/WorryBoardRepository.java
@@ -1,6 +1,7 @@
 package com.example.mssaem_backend.domain.worryboard;
 
 import com.example.mssaem_backend.domain.mbti.MbtiEnum;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,6 +15,8 @@ public interface WorryBoardRepository extends JpaRepository<WorryBoard, Long> {
     Page<WorryBoard> findByMemberId(Long memberId, Pageable pageable);
 
     Page<WorryBoard> findBySolveMemberId(Long memberId, Pageable pageable);
+
+    List<WorryBoard> findTop7ByStateFalseOrderByCreatedAtDesc();
 
     @Query("SELECT wb FROM WorryBoard wb WHERE wb.state = :state AND (:fromMbti IS NULL OR wb.member.mbti = :fromMbti) AND (:toMbti IS NULL OR wb.targetMbti = :toMbti)")
     Page<WorryBoard> findWorriesByStateAndBothMbti(
