7bd6d0c15c450ab5bb5780dc9127cc2b87a644ae
SewoongPark <lympsw12@naver.com>
2023-07-24 15:56:56 +0900

    Fix : EC2에서 Redis 접근에러 해결 V2

--- src/main/java/com/example/mssaem_backend/global/config/redis/RedisConfig.java
+++ src/main/java/com/example/mssaem_backend/global/config/redis/RedisConfig.java
@@ -1,15 +1,30 @@
 package com.example.mssaem_backend.global.config.redis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig {
+
+  @Value("${spring.data.redis.host}")
+  private String redisHost;
+
+  @Value("${spring.data.redis.port}")
+  private int redisPort;
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    return new LettuceConnectionFactory(redisHost, redisPort);
+  }
 
   /**
    * redis pub/sub 메시지를 처리하는 listener 설정
--- src/main/java/com/example/mssaem_backend/global/config/websocket/WebSocketConfig.java
+++ src/main/java/com/example/mssaem_backend/global/config/websocket/WebSocketConfig.java
@@ -21,7 +21,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/stomp/chat")
-        .setAllowedOriginPatterns("*");
+        .setAllowedOriginPatterns("*")
+        .withSockJS();
 
   }
 
