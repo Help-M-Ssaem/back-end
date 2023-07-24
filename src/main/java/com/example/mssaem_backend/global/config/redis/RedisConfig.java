dec15e516a27e280014674d17a801601a1422ad8
SewoongPark <lympsw12@naver.com>
2023-07-24 20:27:32 +0900

    Fix : Redis 에러 해결

--- src/main/java/com/example/mssaem_backend/global/config/redis/RedisConfig.java
+++ src/main/java/com/example/mssaem_backend/global/config/redis/RedisConfig.java
@@ -4,17 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@EnableRedisRepositories
+@Configuration
 public class RedisConfig {
 
   @Value("${spring.data.redis.host}")
