0e53d80571750048ad0ca6e77f8176519c13b903
SewoongPark <lympsw12@naver.com>
2023-07-24 20:16:57 +0900

    Docs : Entity 추가

---
+++ src/main/java/com/example/mssaem_backend/domain/search/SearchController.java
@@ -0,0 +1,10 @@
+package com.example.mssaem_backend.domain.search;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class SearchController {
+
+}
