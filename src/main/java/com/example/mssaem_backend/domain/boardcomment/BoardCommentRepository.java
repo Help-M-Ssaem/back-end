99bdef37517d8e5e2311298bf4075957806bb969
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2023-07-28 23:48:33 +0900

    게시글 댓글 조회 API (#105)
    * Feat: 특정 게시글 상세 조회시 댓글 전체 조회
    
    페이징 방식으로 구현
    
    #29
    
    * Docs: BoardComment Entity 에서 depth,orders 삭제
    
    #29
    
    * Feat: 특정 멤버별 게시글 댓글 전체 조회
    
    #29
    
    * Fix: 특정 멤버별 댓글 조회 기능 삭제
    
    #29
    
    * Fix: 댓글 좋아요 확인 처리 버그 수정
    
    isLiked 값이 제대로 뜨지 않는 오류를 해결
    
    #29
    
    * Fix: 쿼리문 개선, Member Entity 수정에 따른 변경
    
    #29
    
    * Style: URL 수정
    
    #29

--- src/main/java/com/example/mssaem_backend/domain/boardcomment/BoardCommentRepository.java
+++ src/main/java/com/example/mssaem_backend/domain/boardcomment/BoardCommentRepository.java
@@ -1,12 +1,18 @@
 package com.example.mssaem_backend.domain.boardcomment;
 
 import com.example.mssaem_backend.domain.board.Board;
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BoardCommentRepository extends JpaRepository<BoardComment, Long> {
 
     Long countByBoardAndStateTrue(Board board);
 
-    List<BoardComment> findAllByBoardId(Long id);
+
+    @Query(value = "select bc from BoardComment bc join fetch bc.member where bc.board.id = :id and bc.state = true ", countQuery = "select count(bc) from BoardComment bc")
+    Page<BoardComment> findAllByBoardIdAndStateIsTrue(@Param("id") Long id, Pageable pageable);
+
 }
--- src/main/java/com/example/mssaem_backend/domain/boardcommentlike/BoardCommentLikeRepository.java
+++ src/main/java/com/example/mssaem_backend/domain/boardcommentlike/BoardCommentLikeRepository.java
@@ -1,7 +1,10 @@
 package com.example.mssaem_backend.domain.boardcommentlike;
 
+import com.example.mssaem_backend.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardCommentLikeRepository extends JpaRepository<BoardCommentLike, Long> {
+
+    Boolean existsBoardCommentLikeByMemberAndStateIsTrueAndBoardCommentId(Member member , Long boardCommentId);
 
 }
