c7fdc969e56fc9b146b61da617176ebd9919b79c
gourderased <117848386+gourderased@users.noreply.github.com>
2023-07-28 18:46:21 +0900

    토론글 생성, 수정, 삭제 API  (#109)
    * Feat : 토론글 생성 API 구현
    
    * Feat : 토론글 수정 API 구현
    
    discussionOption 생성을 discussionService가 아닌 discussionOptionService에서 하도록 변경
    
    #32 
    
    * Feat : 토론글 삭제 API 구현
    
    #32
    
    * Fix : 머지 충돌 해결
    
    * Fix : merge 충돌(엔티티 괄호) 해결

--- src/main/java/com/example/mssaem_backend/domain/discussionoption/DiscussionOption.java
+++ src/main/java/com/example/mssaem_backend/domain/discussionoption/DiscussionOption.java
@@ -5,6 +5,7 @@ import com.example.mssaem_backend.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
@@ -31,4 +32,10 @@ public class DiscussionOption extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     private Discussion discussion;
 
+    @Builder
+    public DiscussionOption(String content, String imgUrl, Discussion discussion) {
+        this.content = content;
+        this.imgUrl = imgUrl;
+        this.discussion = discussion;
+    }
 }
