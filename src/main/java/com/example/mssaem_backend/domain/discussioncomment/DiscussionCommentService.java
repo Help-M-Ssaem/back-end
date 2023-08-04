3f7e48f84b356f045e7224a14c18e16611170050
yulee <71774564+uri010@users.noreply.github.com>
2023-08-04 16:53:36 +0900

    알림 관련 API  (#138)
    Feat : 알림 관련 API #135

--- src/main/java/com/example/mssaem_backend/domain/discussioncomment/DiscussionCommentService.java
+++ src/main/java/com/example/mssaem_backend/domain/discussioncomment/DiscussionCommentService.java
@@ -10,6 +10,8 @@ import com.example.mssaem_backend.domain.discussioncomment.dto.DiscussionComment
 import com.example.mssaem_backend.domain.discussioncommentlike.DiscussionCommentLikeRepository;
 import com.example.mssaem_backend.domain.member.Member;
 import com.example.mssaem_backend.domain.member.dto.MemberResponseDto.MemberSimpleInfo;
+import com.example.mssaem_backend.domain.notification.NotificationService;
+import com.example.mssaem_backend.domain.notification.TypeEnum;
 import com.example.mssaem_backend.global.common.dto.PageResponseDto;
 import com.example.mssaem_backend.global.config.exception.BaseException;
 import com.example.mssaem_backend.global.config.exception.errorCode.DiscussionErrorCode;
@@ -31,8 +33,10 @@ public class DiscussionCommentService {
     private final DiscussionCommentRepository discussionCommentRepository;
     private final DiscussionCommentLikeRepository discussionCommentLikeRepository;
     private final DiscussionRepository discussionRepository;
+    private final NotificationService notificationService;
 
-    public List<DiscussionCommentSimpleInfo> setDiscussionCommentSimpleInfo(List<DiscussionComment> discussionComments,
+    public List<DiscussionCommentSimpleInfo> setDiscussionCommentSimpleInfo(
+        List<DiscussionComment> discussionComments,
         Member viewer) {
         List<DiscussionCommentSimpleInfo> discussionCommentSimpleInfoList = new ArrayList<>();
 
@@ -45,7 +49,8 @@ public class DiscussionCommentService {
                         discussionComment.getMember())) //해당 게시글을 보는 viewer 와 해당 댓글의 작성자와 같은지 확인
                     .isLiked(
                         discussionCommentLikeRepository.existsDiscussionCommentLikeByMemberAndStateIsTrueAndDiscussionCommentId(
-                            discussionComment.getMember(), discussionComment.getId())) //댓글 좋아요 눌렀는지 안눌렀는지 확인
+                            discussionComment.getMember(),
+                            discussionComment.getId())) //댓글 좋아요 눌렀는지 안눌렀는지 확인
                     .memberSimpleInfo(
                         new MemberSimpleInfo(
                             discussionComment.getMember().getId(),
@@ -65,7 +70,8 @@ public class DiscussionCommentService {
         Member viewer, Long DiscussionId, int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
 
-        Page<DiscussionComment> result = discussionCommentRepository.findAllByDiscussionId(DiscussionId,
+        Page<DiscussionComment> result = discussionCommentRepository.findAllByDiscussionId(
+            DiscussionId,
             pageable);
 
         return new PageResponseDto<>(
@@ -88,12 +94,38 @@ public class DiscussionCommentService {
 
         //만약 코멘트가 존재한다면 그 댓글에 대댓글
         if (discussionCommentRepository.existsDiscussionCommentById(commentId)) {
-            discussionCommentRepository.save(
-                new DiscussionComment(postDiscussionCommentReq.getContent(), member, discussion,
+            DiscussionComment newDiscussionComment = discussionCommentRepository.save(
+                new DiscussionComment(
+                    postDiscussionCommentReq.getContent(),
+                    member,
+                    discussion,
                     commentId.intValue()));
+            // 글을 쓴 멤버가 아닌 멤버가 댓글을 달 때만 알림 등록
+            if (!discussion.getId().equals(member.getId())) {
+                notificationService.createNotification(
+                    newDiscussionComment.getId(),
+                    postDiscussionCommentReq.getContent(),
+                    TypeEnum.REPLY_OF_COMMENT,
+                    discussion.getMember()
+                );
+            }
         } else { //존재하지 않다면 새로운 댓글
-            discussionCommentRepository.save(
-                new DiscussionComment(postDiscussionCommentReq.getContent(), member, discussion, 0));
+            DiscussionComment newDiscussionComment = discussionCommentRepository.save(
+                new DiscussionComment(
+                    postDiscussionCommentReq.getContent(),
+                    member,
+                    discussion,
+                    0)
+            );
+            // 글을 쓴 멤버가 아닌 멤버가 댓글을 달 때만 알림 등록
+            if (!discussion.getId().equals(member.getId())) {
+                notificationService.createNotification(
+                    newDiscussionComment.getId(),
+                    postDiscussionCommentReq.getContent(),
+                    TypeEnum.DISCUSSION_COMMENT,
+                    discussion.getMember()
+                );
+            }
         }
         return true;
     }
@@ -116,7 +148,8 @@ public class DiscussionCommentService {
     }
 
     // 좋아요 수 10개 이상으로 베스트 댓글 3개 조회
-    public List<DiscussionCommentSimpleInfo> findDiscussionCommentBestListByDiscussionId(Member viewer,
+    public List<DiscussionCommentSimpleInfo> findDiscussionCommentBestListByDiscussionId(
+        Member viewer,
         Long discussionId) {
         PageRequest pageRequest = PageRequest.of(0, 3);
 
--- src/main/java/com/example/mssaem_backend/domain/notification/NotificationController.java
+++ src/main/java/com/example/mssaem_backend/domain/notification/NotificationController.java
@@ -1,9 +1,65 @@
 package com.example.mssaem_backend.domain.notification;
 
+import com.example.mssaem_backend.domain.member.Member;
+import com.example.mssaem_backend.domain.notification.dto.NotificationResponseDto.NotificationInfo;
+import com.example.mssaem_backend.global.common.dto.PageResponseDto;
+import com.example.mssaem_backend.global.config.security.auth.CurrentMember;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    /**
+     * 알림 목록 조회
+     */
+    @GetMapping("/member/notifications")
+    public ResponseEntity<PageResponseDto<List<NotificationInfo>>> findNotifications(
+        @CurrentMember Member member,
+        @RequestParam int page,
+        @RequestParam int size) {
+        return ResponseEntity.ok(notificationService.findNotifications(member, page, size));
+    }
+
+    /**
+     * 알림 읽기
+     */
+    @PatchMapping("/member/notifications")
+    public ResponseEntity<String> checkNotification(@RequestParam Long id) {
+        return ResponseEntity.ok(notificationService.checkNotification(id));
+    }
+
+    /**
+     * 알림 전체 읽기
+     */
+    @PatchMapping("/member/notifications/all")
+    public ResponseEntity<String> checkAllNotifications(@CurrentMember Member member) {
+        return ResponseEntity.ok(notificationService.checkAllNotifications(member));
+    }
+
+    /**
+     * 알림 삭제하기 (읽은 알림만 가능)
+     */
+    @DeleteMapping("/member/notifications")
+    public ResponseEntity<String> deleteNotification(@RequestParam Long id) {
+        return ResponseEntity.ok(notificationService.deleteNotification(id));
+    }
+
+    /**
+     * 알림 전체 삭제하기
+     */
+    @DeleteMapping("/member/notifications/all")
+    public ResponseEntity<String> deleteAllNotification(@CurrentMember Member member) {
+        return ResponseEntity.ok(notificationService.deleteAllNotifications(member));
+    }
+
 }
