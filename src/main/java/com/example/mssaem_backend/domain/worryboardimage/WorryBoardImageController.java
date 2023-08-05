6ce8c1a25a15d7316103d86c7e45a05a4c330450
gourderased <117848386+gourderased@users.noreply.github.com>
2023-08-06 00:38:13 +0900

    고민글 파일 업로드 API 분리 (#142)
    * Fix : 파입 업로드 방식 변경에 따른 수정
    
    - 고민 게시글 생성/수정과 이미지 업로드 API 분리
    
    * Fix : 코드 정리

--- src/main/java/com/example/mssaem_backend/domain/worryboardimage/WorryBoardImageController.java
+++ src/main/java/com/example/mssaem_backend/domain/worryboardimage/WorryBoardImageController.java
@@ -1,9 +1,21 @@
 package com.example.mssaem_backend.domain.worryboardimage;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RestController
 public class WorryBoardImageController {
+
+    private final WorryBoardImageService worryBoardImageService;
+
+    @PostMapping("/member/worry-boards/files")
+    public ResponseEntity<String> uploadFiles(
+        @RequestPart(value = "image", required = false) MultipartFile multipartFile) {
+        return ResponseEntity.ok(worryBoardImageService.uploadFile(multipartFile));
+    }
 }
