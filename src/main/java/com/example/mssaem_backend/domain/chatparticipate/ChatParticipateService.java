11a74afc5aef79b3238a9a337ad81494fbed8daf
SewoongPark <lympsw12@naver.com>
2023-08-05 23:28:09 +0900

    Feat : 채팅방 조회 Response 변경 #145

--- src/main/java/com/example/mssaem_backend/domain/chatparticipate/ChatParticipateService.java
+++ src/main/java/com/example/mssaem_backend/domain/chatparticipate/ChatParticipateService.java
@@ -1,9 +1,13 @@
 package com.example.mssaem_backend.domain.chatparticipate;
 
+import com.example.mssaem_backend.domain.chatmessage.ChatMessageRepository;
 import com.example.mssaem_backend.domain.chatparticipate.dto.ChatParticipateResponseDto.ChatParticipateRes;
 import com.example.mssaem_backend.domain.chatroom.ChatRoom;
 import com.example.mssaem_backend.domain.member.Member;
 import com.example.mssaem_backend.domain.member.MemberRepository;
+import com.example.mssaem_backend.domain.member.dto.MemberResponseDto.MemberSimpleInfo;
+import com.example.mssaem_backend.global.config.exception.BaseException;
+import com.example.mssaem_backend.global.config.exception.errorCode.ChatRoomParticipateErrorCode;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class ChatParticipateService {
 
   private final ChatParticipateRepository chatParticipateRepository;
+  private final ChatMessageRepository chatMessageRepository;
   private final MemberRepository memberRepository;
 
   public void insertChatParticipate(String sessionID, ChatRoom chatRoom, String memberName) {
@@ -25,13 +30,20 @@ public class ChatParticipateService {
   }
 
   public List<ChatParticipateRes> selectChatRooms(Member member) {
-    List<ChatParticipate> allByMember = chatParticipateRepository.findAllByMember(member);
-    return allByMember.stream()
-        .map(ChatParticipateRes::new).collect(Collectors.toList());
+    List<ChatParticipate> result = chatParticipateRepository.findAllByMember(member);
+
+    if (result.isEmpty()) {
+      throw new BaseException(ChatRoomParticipateErrorCode.EMPTY_CHATPARTICIPATE);
+    }
+
+    return result.stream()
+        .map(r -> new ChatParticipateRes(r,
+            new MemberSimpleInfo(r.getMember(), r.getMember().getBadgeName()),
+            chatMessageRepository.selectByChatRoom(r.getChatRoom()))).collect(Collectors.toList());
   }
 
   @Transactional
-  public void deleteChatParticipate(String sessionId){
+  public void deleteChatParticipate(String sessionId) {
     ChatParticipate chatParticipate = chatParticipateRepository.findBySessionId(sessionId);
     chatParticipateRepository.delete(chatParticipate);
   }
