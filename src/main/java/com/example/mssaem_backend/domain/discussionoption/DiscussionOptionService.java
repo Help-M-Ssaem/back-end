2d6b6c0e6e22ecfc743eab20aee38c8760385aa8
gourderased <gourderased@gmail.com>
2023-08-09 20:15:03 +0900

    Fix : 토론글 수정/삭제 권한 변경
    #161

--- src/main/java/com/example/mssaem_backend/domain/discussionoption/DiscussionOptionService.java
+++ src/main/java/com/example/mssaem_backend/domain/discussionoption/DiscussionOptionService.java
@@ -48,6 +48,8 @@ public class DiscussionOptionService {
                 s3Service.deleteFile(s3Service.parseFileName(discussionOption.getImgUrl()));
             }
         }
+        //해당 discussionOption에 존재하는 discussionOptionSelected 삭제
+
         //해당 discussion에 존재하는 option 삭제
         discussionOptionRepository.deleteAllByDiscussion(discussion);
     }
