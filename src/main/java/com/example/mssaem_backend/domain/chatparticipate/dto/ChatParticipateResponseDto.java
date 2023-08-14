d0480b26b22eb128192e79e0775cc488fdf01d48
SewoongPark <lympsw12@naver.com>
2023-08-15 01:32:21 +0900

    Docs : 채팅방 모두 조회 API 결과 변경
    #201

--- src/main/java/com/example/mssaem_backend/domain/chatparticipate/dto/ChatParticipateResponseDto.java
+++ src/main/java/com/example/mssaem_backend/domain/chatparticipate/dto/ChatParticipateResponseDto.java
@@ -3,6 +3,7 @@ package com.example.mssaem_backend.domain.chatparticipate.dto;
 import com.example.mssaem_backend.domain.chatmessage.ChatMessage;
 import com.example.mssaem_backend.domain.chatparticipate.ChatParticipate;
 import com.example.mssaem_backend.domain.member.dto.MemberResponseDto.MemberSimpleInfo;
+import com.example.mssaem_backend.domain.worryboard.WorryBoard;
 import com.example.mssaem_backend.global.common.Time;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,13 +20,19 @@ public class ChatParticipateResponseDto {
     private boolean state;
     private String lastMessage;
     private String lastSendAt;
+    private String chatRoomTitle;
+    private String memberMbti;
+    private String targetMbti;
     private MemberSimpleInfo memberSimpleInfo;
 
-    public ChatParticipateRes(ChatParticipate chatParticipate, MemberSimpleInfo memberSimpleInfo, ChatMessage message) {
+    public ChatParticipateRes(ChatParticipate chatParticipate, MemberSimpleInfo memberSimpleInfo, ChatMessage message, WorryBoard worryBoard) {
       this.chatRoomId = chatParticipate.getChatRoom().getId();
       this.state = chatParticipate.getChatRoom().isState();
       this.lastMessage = message == null ? "" : message.getMessage();
       this.lastSendAt = message == null ? "" : Time.calculateTime(message.getCreatedAt(), 3);
+      this.chatRoomTitle = chatParticipate.getChatRoom().getTitle();
+      this.memberMbti = chatParticipate.getMember().getDetailMbti();
+      this.targetMbti = worryBoard.getTargetMbti().toString();
       this.memberSimpleInfo = memberSimpleInfo;
     }
 
--- src/main/java/com/example/mssaem_backend/domain/chatroom/ChatRoomRepository.java
+++ src/main/java/com/example/mssaem_backend/domain/chatroom/ChatRoomRepository.java
@@ -1,7 +1,12 @@
 package com.example.mssaem_backend.domain.chatroom;
 
+import com.example.mssaem_backend.domain.worryboard.WorryBoard;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-
+    @Query("select wb from WorryBoard wb join ChatRoom cr on cr.worryBoardId = wb.id where cr.id in (:chatRoomIds)")
+    List<WorryBoard> findWorryBoardAllByChatRoom(@Param("chatRoomIds") List<Long> chatRoomIds);
 }
