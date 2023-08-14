bf644908f0ff98689e06a3fcc859e58eea4a3a82
SewoongPark <lympsw12@naver.com>
2023-08-14 19:24:54 +0900

    Fix : 에러 이름 변경 #194

--- src/main/java/com/example/mssaem_backend/global/config/exception/errorCode/ChatRoomParticipateErrorCode.java
+++ src/main/java/com/example/mssaem_backend/global/config/exception/errorCode/ChatRoomParticipateErrorCode.java
@@ -11,7 +11,7 @@ public enum ChatRoomParticipateErrorCode implements ErrorCode {
 
   EMPTY_CHATPARTICIPATE("CHATROOM_001", "참여한 채팅방이 없습니다.", HttpStatus.NOT_FOUND),
   FULL_CHATROOM("CHATROOM_002", "채팅방이 가득 찼습니다", HttpStatus.CONFLICT),
-  FULL_PARTICIPATE("CHATROOM_003", "이미 채팅방에 입장 했습니다", HttpStatus.CONFLICT);
+  DUPLICATE_PARTICIPATE("CHATROOM_003", "이미 채팅방에 입장 했습니다", HttpStatus.CONFLICT);
   private final String errorCode;
   private final String message;
   private final HttpStatus status;
