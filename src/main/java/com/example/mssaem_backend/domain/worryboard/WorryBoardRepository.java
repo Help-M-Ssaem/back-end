0c2919c364c7d315db7516573e79fc87f71119ad
yulee <dbfl0461@gmail.com>
2023-08-21 18:10:33 +0900

    Fix : 검색 API

--- src/main/java/com/example/mssaem_backend/domain/worryboard/WorryBoardRepository.java
+++ src/main/java/com/example/mssaem_backend/domain/worryboard/WorryBoardRepository.java
@@ -15,11 +15,15 @@ import org.springframework.data.repository.query.Param;
 public interface WorryBoardRepository extends JpaRepository<WorryBoard, Long> {
 
     @Query("SELECT wb FROM WorryBoard wb WHERE wb.state = true AND wb.isSolved = :isSolved AND ( :worryBoardId IS NULL OR wb.id <> :worryBoardId) order by wb.createdAt desc")
-    Page<WorryBoard> findByIsSolvedAndStateTrueOrderByCreatedAtDesc(@Param("isSolved") boolean isSolved , @Param("worryBoardId") Long worryBoardId, Pageable pageable);
+    Page<WorryBoard> findByIsSolvedAndStateTrueOrderByCreatedAtDesc(
+        @Param("isSolved") boolean isSolved, @Param("worryBoardId") Long worryBoardId,
+        Pageable pageable);
 
-    Page<WorryBoard> findByMemberIdAndStateTrueOrderByCreatedAtDesc(Long memberId, Pageable pageable);
+    Page<WorryBoard> findByMemberIdAndStateTrueOrderByCreatedAtDesc(Long memberId,
+        Pageable pageable);
 
-    Page<WorryBoard> findBySolveMemberIdAndStateTrueOrderByCreatedAtDesc(Long memberId, Pageable pageable);
+    Page<WorryBoard> findBySolveMemberIdAndStateTrueOrderByCreatedAtDesc(Long memberId,
+        Pageable pageable);
 
     List<WorryBoard> findTop7ByIsSolvedFalseAndStateTrueOrderByCreatedAtDesc();
 
@@ -38,14 +42,15 @@ public interface WorryBoardRepository extends JpaRepository<WorryBoard, Long> {
         @Param("oneMonthAgo") LocalDateTime oneMonthAgo, PageRequest pageRequest);
 
     // 고민글 검색하기
-    @Query("SELECT wb FROM WorryBoard wb WHERE"
-        + "(    (:type = 0 AND (LOWER(wb.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(wb.content) LIKE LOWER(CONCAT('%', :keyword, '%'))))"
-        + " OR (:type = 1 AND LOWER(wb.title) LIKE LOWER(CONCAT('%', :keyword, '%')))"
-        + " OR (:type = 2 AND LOWER(wb.content) LIKE LOWER(CONCAT('%', :keyword, '%')))"
-        + " OR (:type = 3 AND LOWER(wb.member.nickName) LIKE LOWER(CONCAT('%', :keyword, '%'))) )"
-        + " AND wb.isSolved = :isSolved AND (:fromMbti IS NULL OR wb.member.mbti = :fromMbti) AND (:toMbti IS NULL OR wb.targetMbti = :toMbti)ORDER BY wb.createdAt DESC ")
+    @Query(value = "SELECT wb FROM WorryBoard wb WHERE"
+        + "(    (:searchType = 0 AND (LOWER(wb.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(wb.content) LIKE LOWER(CONCAT('%', :keyword, '%'))))"
+        + " OR (:searchType = 1 AND LOWER(wb.title) LIKE LOWER(CONCAT('%', :keyword, '%')))"
+        + " OR (:searchType = 2 AND LOWER(wb.content) LIKE LOWER(CONCAT('%', :keyword, '%')))"
+        + " OR (:searchType = 3 AND LOWER(wb.member.nickName) LIKE LOWER(CONCAT('%', :keyword, '%'))) )"
+        + " AND wb.isSolved = :isSolved AND (:fromMbti IS NULL OR wb.member.mbti = :fromMbti) AND (:toMbti IS NULL OR wb.targetMbti = :toMbti)ORDER BY wb.createdAt DESC",
+        countQuery = "SELECT COUNT(w) FROM WorryBoard w")
     Page<WorryBoard> searchWorriesBySolvedAndTypeAndMbti(
-        @Param("type") int type,
+        @Param("searchType") int searchType,
         @Param("keyword") String keyword,
         @Param("isSolved") Boolean isSolved,
         @Param("fromMbti") MbtiEnum fromMbti,
